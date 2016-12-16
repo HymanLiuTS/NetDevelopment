76ec2f37d68e1030f21fcf3dd1ee91bceabc29d0
刘志辉 <879651072@qq.com>
2016-12-16 08:47:57 +0800

    网络编程（38）—— 利用互斥mutex控制多线程对临界区的访问

--- mutextTs.c
+++ mutextTs.c
@@ -1,26 +1,36 @@
 #include<stdio.h>
 #include<pthread.h>
 int num=0;
+pthread_mutex_t mutex;
 void* pthread_main1()
 {
     int i=0;
+    pthread_mutex_lock(&mutex);
     for(i=0;i<1000000000;i++)
     {
-         num++;
+        --num;
     }
+    pthread_mutex_unlock(&mutex);
 }
 
 void* pthread_main2()
 {
     int i=0;
+    pthread_mutex_lock(&mutex);
     for(i=0;i<1000000000;i++)
     {
-        num--;
-    } 
+        ++num;
+    }
+    pthread_mutex_unlock(&mutex);
 }
+
+
+
 int main()
 {
     int i=0;
+    pthread_mutex_init(&mutex,NULL);
+    printf("mutex=%d\n",mutex);
     pthread_t pid[2];
     pthread_create((void*)&(pid[0]),NULL,(void*)pthread_main1,NULL);
     pthread_create((void*)&(pid[1]),NULL,(void*)pthread_main2,NULL);
@@ -29,6 +39,7 @@ int main()
         pthread_join(pid[i],NULL);
     }
     printf("%d\n",num);
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
 
